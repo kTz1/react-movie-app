9cdcf082a0ac39a206469dfb848967f08b02eb9a
Simon Catalin <simoncatalin@Simons-MacBook-Air.local>
2021-11-01 12:06:07 +0200

    added login and rate movie component

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
+import Login from './components/Login';
+// Context
+import UserProvider from './context';
 //Styles
 import { GlobalStyle } from './GlobalStyle';
 
 
 const App = () => (
-    <Router>
-        <Header />
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/:movieId' element={<Movie />} />
-            <Route path='/*' element={<NotFound />} />
-        </Routes>
-        <GlobalStyle />
-    </Router>
+    <UserProvider>
+        <Router>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/:movieId' element={<Movie />} />
+                <Route path='/*' element={<NotFound />} />
+            </Routes>
+            <GlobalStyle />
+        </Router>
+    </UserProvider>
 );
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,34 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 // Logo
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
+// Context
+import { Context } from '../../context';
 // Styles
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './header.styles';
 
-const Header = () => (
-    <Wrapper>
-        <Content>
-        <Link to='/'>
-            <LogoImg src={RMDBLogo} alt='rmdb-logo' />
-        </Link>
+const Header = () => {
+    const [user] = useContext(Context);
+    console.log(user);
+
+    return (
+        <Wrapper>
+            <Content>
+            <Link to='/'>
+                <LogoImg src={RMDBLogo} alt='rmdb-logo' />
+            </Link>
+            {user ? (
+                <span>Logged in as: {user.username}</span>
+            ) : (
+                <Link to='/login'>
+                    <span>Login</span>
+                </Link>
+            )}           
             <TMDBLogoImg src={TMDBLogo} alt='tmbd-logo' />
-        </Content>
-    </Wrapper>
-);
+            </Content>
+        </Wrapper>
+    );
+};
 
 export default Header;
